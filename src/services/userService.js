d5b66df869e5d0bd7763f2c44352d0b80f53578d
huy-get <vovanhuy7497@gmail.com>
2023-08-30 12:04:22 +0700

    note ghi chu

--- src/services/userService.js
+++ src/services/userService.js
@@ -6,8 +6,9 @@ import axios from '../axios';// axios de goi den sever nodejs ben phia khach
 const handleLoginAPI =(userEmail,userPassword) =>{
      return axios.post('http://localhost:3000/api/login',{ email:userEmail, password:userPassword });
 }
-
+//
 const getAllUsers = (InputId) =>{
+     //template string
      return axios.get(`http://localhost:3000/api/get-all-user?id=${InputId}`)
 }
 export {handleLoginAPI,getAllUsers};
